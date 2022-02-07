485b4f5524674cf947eb1f7e26bc760b7a6f912b
byronicone <byron.barnes@afreshtechnologies.com>
2022-02-07 10:17:43 -0500

    Using variables in query example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { React, useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import Organization from "./Organization";
 
 const axiosGitHubGraphQL = axios.create({
-  baseURL: "https://api.github.com/graphql",
+  baseURL: "https://api.github.com",
   headers: {
     Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
   },
@@ -11,13 +12,50 @@ const axiosGitHubGraphQL = axios.create({
 
 const TITLE = "React GraphQL GitHub Client";
 
+const GET_ISSUES_OF_REPOSITORY = `
+query ($organization: String!, $repository: String!) {
+  organization(login: $organization) {
+    name
+    url
+    repository(name: $repository) {
+      name
+      url
+      issues(last: 5){
+        edges{
+          node{
+            id
+            title
+            url
+          }
+        }
+      }
+    }
+  }
+}`;
+
 function App() {
   const defaultPath = "the-road-to-learn-react/the-road-to-learn-react";
   const [path, setPath] = useState(defaultPath);
+  const [organization, setOrganization] = useState();
+  const [errors, setErrors] = useState();
 
   useEffect(() => {
-    //fetch data
+    onFetchFromGithub();
   }, []);
+
+  const getIssuesOfRepository = () => {
+    const [organization, repository] = path.split("/");
+    return axiosGitHubGraphQL.post("/graphql", {
+      query: GET_ISSUES_OF_REPOSITORY,
+      variables: { organization, repository },
+    });
+  };
+
+  const onFetchFromGithub = async () => {
+    let result = await getIssuesOfRepository();
+    setOrganization(result.data.data.organization);
+    setErrors(result.data.errors);
+  };
 
   const onChange = (e) => {
     setPath(e.target.value);
@@ -25,7 +63,7 @@ function App() {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    //fetch data
+    onFetchFromGithub();
   };
 
   return (
@@ -45,7 +83,11 @@ function App() {
 
       <hr />
 
-      {/*Here comes the result! */}
+      {organization ? (
+        <Organization organization={organization} errors={errors} />
+      ) : (
+        <p>No information yet...</p>
+      )}
     </div>
   );
 }
